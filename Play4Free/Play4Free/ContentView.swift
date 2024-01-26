ea7bf39a73917d179b6ab5fad25e803638db6153
SelcukOzkara <se.oezkara@gmail.com>
2024-01-26 11:19:00 +0100

    First commit -> Empty Project

---
+++ Play4Free/Play4Free/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Play4Free
+//
+//  Created by Selcuk Özkara on 26.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Play4Free/Play4Free/Play4FreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Play4FreeApp.swift
+//  Play4Free
+//
+//  Created by Selcuk Özkara on 26.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Play4FreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
